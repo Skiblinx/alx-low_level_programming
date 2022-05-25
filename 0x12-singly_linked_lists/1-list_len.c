ad3b7f0787aac606a8ac7f71932dc758f753593a
Adeleye Abdulbasit <101928663+Skiblinx@users.noreply.github.com>
2022-05-25 03:17:20 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * list_len - is a function that counts the elements of a list_t file
- * @h: is the string input
- * Return: number of nodes
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nelem;
 
-	while (h)
+	nelem = 0;
+	while (h != NULL)
 	{
 		h = h->next;
-		nodes++;
+		nelem++;
 	}
-	return (nodes);
+	return (nelem);
 }
